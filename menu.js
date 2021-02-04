35efde37725f002c411fd469c7be19bb60867890
TbMA13 <74298344+TbMA13@users.noreply.github.com>
2021-02-04 17:05:22 +0200

    Нав меню

--- menu.js
+++ menu.js
@@ -1,14 +1,33 @@
-function open_nav_menu(){
-    $('#nav_background').css('display', 'block');
-    $('.nav-menu').fadeIn("fast");
-    $('.nav-menu').css('display', 'flex');
-    
+function open_nav_menu() {
+    if ($(document).width() >= '686') {
+        document.body.style.overflow = 'hidden';
+        $('#nav_background').css('display', 'block');
+        $('.nav-menu').fadeIn("fast");
+        $('.nav-menu').css('display', 'flex');
+    }
+    if ($(document).width() < '686') {
+        $('main').css('position', 'fixed');
+        $('footer').css('display', 'none');
+        $('#nav_background').css('display', 'block');
+        $('.nav-menu').fadeIn("fast");
+        $('.nav-menu').css('display', 'flex');
+    }
 }
 function close_nav_menu() {
-    $('#nav_background').css('display', 'none');
-
-    $('.nav-menu').fadeOut("fast");
-    $('.nav-menu').css('display', 'flex');
+    if ($(document).width() >= '686') {
+        document.body.style.overflow = 'auto';
+        $('#nav_background').css('display', 'none');
+        $('.nav-menu').fadeOut("fast");
+        $('.nav-menu').css('display', 'flex');
+    }
+    if ($(document).width() < '686') {
+        $('main').css('position', 'relative');
+        $('footer').css('display', 'flex');
+        
+        $('#nav_background').css('display', 'none');
+        $('.nav-menu').fadeOut("fast");
+        $('.nav-menu').css('display', 'flex');
+    }
 }
 //TODO НЕ РАБОТАЕТ НА ТЕЛЕФОНЕ
 /*
