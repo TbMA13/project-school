72f13c0a5933f515d0e542829c88d28c8aa5ba25
TbMA13 <74298344+TbMA13@users.noreply.github.com>
2021-06-19 15:14:17 +0200

    слабовидящие

--- for_invalid.js
+++ for_invalid.js
@@ -1,5 +1,35 @@
-let score = 0
+/*const forInvalidBlock = document.getElementById('for-invalid');
+let i = 0;
 
+function invalid(i, forInvalidBlock) {
+    for (let i; true; i += 1) {
+        if (i === 0) {
+            forInvalidBlock.classList.add('visible');
+        } else if ((i % 2) !== 0) {
+            forInvalidBlock.style.animationDirection = reverse;
+        } else if ((i % 2) !== 1) {
+            forInvalidBlock.style.animationDirection = normal;
+        }
+    }
+}
+*/
+/*function invalid(score) {
+    const forInvalidBlock = document.getElementById('for-invalid');
+    if (score === 0) {
+        score += 1;
+        forInvalidBlock.classList.add('visible');
+    }
+    else if ((score % 2) !== 0) {
+        score += 1;
+        forInvalidBlock.style.animationDirection = reverse;
+    }
+    else if ((score % 2) !== 1) {
+        score += 1;
+        forInvalidBlock.style.animationDirection = normal;
+    }
+}
+*/
+/*
 function choiseInvalidBlock(score) {
     if (score === 0) {
         hideInvalidBlock();
@@ -11,6 +41,7 @@ function choiseInvalidBlock(score) {
 function hideInvalidBlock() {
     score += 1;
     const forInvalidBlock = document.getElementById('for-invalid');
+    forInvalidBlock.classList.remove('visible');
     forInvalidBlock.classList.add('hide');
 }
 
@@ -18,4 +49,5 @@ function visionInvalidBlock() {
     score -= 1;
     const forInvalidBlock = document.getElementById('for-invalid');
     forInvalidBlock.classList.remove('hide');
-}
+    forInvalidBlock.classList.add('visible');
+}*/
